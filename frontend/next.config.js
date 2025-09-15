fd3039010147fed79c9deac45f881f2c3dd0069e
Syed Arfan <b00092529@aus.edu>
2025-09-15 10:14:36 +0300

    Restore frontend files properly

---
+++ frontend/next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  trailingSlash: true,
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'https://thesimpleai.vercel.app',
+  },
+};
+
+module.exports = nextConfig;
