6cf7683c058a7d350fa1ff5b7224155a7de68d85
Syed Arfan <b00092529@aus.edu>
2025-10-29 16:02:57 +0300

    Fix CORS: Allow Cache-Control and Pragma headers
    - Add Cache-Control and Pragma to Access-Control-Allow-Headers
    - Fixes CORS preflight errors when checking Outlook connection

--- backend/server.js
+++ backend/server.js
@@ -142,7 +142,7 @@ app.use((req, res, next) => {
   
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS,PATCH');
-  res.header('Access-Control-Allow-Headers', 'Origin,X-Requested-With,Content-Type,Accept,Authorization,X-Request-ID,X-Admin-Secret');
+  res.header('Access-Control-Allow-Headers', 'Origin,X-Requested-With,Content-Type,Accept,Authorization,X-Request-ID,X-Admin-Secret,Cache-Control,Pragma');
   res.header('Access-Control-Expose-Headers', 'Content-Length,X-Request-ID');
   
   if (req.method === 'OPTIONS') {
